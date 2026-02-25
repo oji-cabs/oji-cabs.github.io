d9980df397abdee1739c81f3ddd07c7cae03205c
numberbee7070 <yashgarg2910@gmail.com>
2026-02-25 11:21:33 +0530

    add robots.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: "https://oji-cabs.github.io",
+
   redirects: {
     "/meterraja-privacy-policy/": "/policies/meter_raja/",
   },
+
+  integrations: [sitemap()],
 });
